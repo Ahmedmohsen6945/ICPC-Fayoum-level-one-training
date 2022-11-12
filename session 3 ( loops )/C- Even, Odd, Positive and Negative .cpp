9f3d2de6abdf71e54cc1aa3a57b2e8b70ff6c0af
Ahmed Mohsen <102276002+Ahmedmohsen6945@users.noreply.github.com>
2022-11-12 21:06:44 +0200

    Update C- Even, Odd, Positive and Negative .cpp

--- session 3 ( loops )/C- Even, Odd, Positive and Negative .cpp
+++ session 3 ( loops )/C- Even, Odd, Positive and Negative .cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std ;
 /*                           Even, Odd, Positive and Negative
- * at first we need to in declare six integer variables one of them to get the number of integers from user , another one to get the integers in the loop and the rest of them to count the number of even , odd , positive and negative numbers
- * number of integers from user
- * we can implement any type of loops ( while , for , do while )
- * make a condition for the loop from one to the given number ( 1 : n )
- * get integer from user in every iteration
- * and if this integer is even increase the specific variable by one , and so on with the other types of numbers
- * print the number of even , odd , positive and negative numbers every , value in a single line
-Prepared By: Ahmed Mohsen (group 8)
+ * at first we need to in declare six integer variables one of them to get the number of integers from user , another one to get the integers in the loop and the rest of them to count the number of even , odd , positive and negative numbers.
+ * get the number of integers from user.
+ * we can implement any type of loops ( while , for , do while ).
+ * make a condition for the loop from one to the given number ( 1 : n ).
+ * get integer from user in every iteration.
+ * and if this integer is even increase the specific variable by one , and so on with the other types of numbers.
+ * print the number of even , odd , positive and negative numbers every , value in a single line.
+Prepared By: Ahmed Mohsen (group 8).
 */
 int main () {
     int n, num, ev, od, ne, po;
