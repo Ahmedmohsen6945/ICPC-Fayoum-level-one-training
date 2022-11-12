c2d140e0372efee98f99f2ed85aedb9983e489f7
Ahmed Mohsen <102276002+Ahmedmohsen6945@users.noreply.github.com>
2022-11-12 19:54:00 +0200

    Create A - Summation from 1 to N Easy.cpp

---
+++ session 3 ( loops )/A - Summation from 1 to N Easy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+
+/*                           Summation from 1 to N Easy
+ * at first we need to in declare integer variable to get the value from user
+ * get the value
+ * we can implement any type of loops ( while , for , do while )
+ * make a condition for the loop from on to the given number
+ * print the counter of the loop in it's body and don't forget (<< endl ;) at the end of the line
+ */
+
+int main()
+{
+int n ;
+cin >> n ;
+for (int i = 1 ; i <= n ; i++ )
+    cout << i << endl ;
+}
