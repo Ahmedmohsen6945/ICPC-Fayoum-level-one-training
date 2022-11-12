7f6220cbe3222575e98757256ae46406e0a12eb8
Ahmed Mohsen <102276002+Ahmedmohsen6945@users.noreply.github.com>
2022-11-12 21:08:26 +0200

    Update B - Summation from 1 to N Hard.cpp

--- session 3 ( loops )/B - Summation from 1 to N Hard.cpp
+++ session 3 ( loops )/B - Summation from 1 to N Hard.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std ;
 /*                           Summation from 1 to N Hard ( mathematical solution and super efficient )
- * at first we need to in declare integer variable to get the value from user
- * get the value
- * and we just use the formela (n*(n+1))/2 to get the summation
- * and then print it
- Prepared By: Ahmed Mohsen (group 8)
+ * at first we need to in declare integer variable to get the value from user.
+ * get the value.
+ * and we just use the formela (n*(n+1))/2 to get the summation.
+ * and then print it.
+ Prepared By: Ahmed Mohsen (group 8).
  */
 int main (){
 long long n ;
@@ -13,12 +13,12 @@ cin >> n ;
 cout << (n *( n + 1)) /2 ;
 }
 /*                           Summation from 1 to N Hard ( the easy way but not efficient )
- * at first we need to in declare integer variable to get the value from user
- * get the value
- * we can implement any type of loops ( while , for , do while )
- * we need too another variable of long long type  initialize it with zero
- * make a condition for the loop from one to the given number ( 1 : n )
- * print the summation of all numbers form one to the given number
+ * at first we need to in declare integer variable to get the value from user.
+ * get the value.
+ * we can implement any type of loops ( while , for , do while ).
+ * we need too another variable of long long type  initialize it with zero.
+ * make a condition for the loop from one to the given number ( 1 : n ).
+ * print the summation of all numbers form one to the given number.
 
 int main()
 {
